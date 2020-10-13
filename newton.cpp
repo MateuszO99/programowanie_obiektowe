4a7b03fb67928b0ad5c72b22dabd7311111faa3f
DonLuq <lbs_macos@LBs-MacOSs-MacBook-Air.local>
2020-10-13 17:24:08 +0200

    Indent-ready

--- newton.cpp
+++ newton.cpp
@@ -2,20 +2,34 @@
 
 using namespace std;
 
-long long sil(int n){
+long long sil(int n)
+//funkcja obliczajaca silinie n!
+{
 
-long long silnia = 1;
-for(int i=n;i>1;i--)
-		silnia*=i;
-		
-return silnia;
+	long long silnia = 1;
+	for (int i = n; i > 1; i--)
+	{
+		silnia *= i;
+	}
+
+	return silnia;
 }
 
-long long newton(int n, int k){
-long long nt = sil(n)/(sil(k)*sil(n-k));
+long long newton(int n, int k)
+//funkcja obliczajaca C(n,k) wykorzystujac funkcje sil()
+{
+	if (n or k < 0 or n < k)
+	{
+		//zglosic wyjatek
+	}
+	long long nt = sil(n) / (sil(k) * sil(n - k));
 
-return nt;
+	return nt;
 }
 
-int main(){return 0;}
-
+int main()
+{
+	cout << sil(5);
+	cout << newton(5, 2); 
+	return 0;
+}
