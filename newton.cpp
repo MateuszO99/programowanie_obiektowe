4a138eab2747387efcbca592201752d60c938008
DonLuq <lbs_macos@LBs-MacOSs-MacBook-Air.local>
2020-10-13 16:52:55 +0200

    Newton dwumian - dodac testy

---
+++ newton.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+long long sil(int n){
+
+long long silnia = 1;
+for(int i=n;i>1;i--)
+		silnia*=i;
+return silnia;
+}
+
+long long newton(int n, int k){
+long long nt = sil(n)/(sil(k)*sil(n-k));
+return nt;
+}
+
+int main(){return 0;}
+
