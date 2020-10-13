e2f44986fa08b6620d3244fbb7df5f25d9b61f11
DonLuq <lbs_macos@LBs-MacOSs-MacBook-Air.local>
2020-10-13 17:00:32 +0200

    indents

--- newton.cpp
+++ newton.cpp
@@ -7,11 +7,13 @@ long long sil(int n){
 long long silnia = 1;
 for(int i=n;i>1;i--)
 		silnia*=i;
+		
 return silnia;
 }
 
 long long newton(int n, int k){
 long long nt = sil(n)/(sil(k)*sil(n-k));
+
 return nt;
 }
 
