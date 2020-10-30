f15c08409685dd270a7e4e8a4368429069bc84c9
DonLuq <61588618+DonLuq@users.noreply.github.com>
2020-10-30 09:58:19 +0100

    wersja z doublami

--- newton.cpp
+++ newton.cpp
@@ -1,32 +1,43 @@
 #include <iostream>
 #include <stdexcept>
-
+#include <array>
+#include <iomanip>
 #include "newton.h"
+
 
 using namespace std;
 
-static long long sil(int n)
-//funkcja obliczajaca silinie n!
+static long double factorial(int n)
 {
-
-	long long silnia = 1;
-	for (int i = n; i > 1; i--)
-	{
-		silnia *= i;
-	}
-
-	return silnia;
+    if(n > 1)
+        return n * factorial(n - 1);
+    else
+        return 1;
 }
 
-long long newton(int n, int k)
+static long double newton(int n, int k)
 //funkcja obliczajaca C(n,k) wykorzystujac funkcje sil()
 {
-	if (n < 0 || k < 0 || n < k)
-	{
-		cout << "Bledne dane wejsciowe wymane n>k dla C(n,k) oraz n,k > 0" << endl;
-		throw std::invalid_argument("Bledne dane wejsciowe wymane n>k dla C(n,k) oraz n,k > 0");
-	}
-	long long nt = sil(n) / (sil(k) * sil(n - k));
-
-	return nt;
+    if (n < 0 || k < 0 || n < k)
+    {
+        cout << "Bledne dane wejsciowe wymane n>k dla C(n,k) oraz n,k > 0" << endl;
+        throw std::invalid_argument("Bledne dane wejsciowe wymane n>k dla C(n,k) oraz n,k > 0");
+    }
+    return factorial(n) / (factorial(k) * factorial(n - k));
 }
+
+// Driver program
+int main()
+{
+//    cout<<factorial(3)<<endl;
+//    cout<<factorial(4)<<endl;
+//    cout<<factorial(5)<<endl;
+//    cout<<factorial(20)<<endl;
+//    cout<<factorial(30)<<endl;
+//    cout<<factorial(40)<<endl;
+    
+    return 0;
+}
+
+
+
--- test.cpp
+++ test.cpp
@@ -1,6 +1,8 @@
 #include <gtest/gtest.h>
-
+#include <iostream>
 #include "newton.h"
+
+using namespace std;
 
 TEST(NewtonTest, simple_ints)
 {
@@ -13,5 +15,9 @@ TEST(NewtonTest, simple_ints)
 int main(int argc, char* argv[])
 {
   ::testing::InitGoogleTest(&argc, argv);
+  cout << std::setprecision(20) << newton(40, 20)<<endl;
+  cout << std::setprecision(20) << newton(40, 40)<<endl;
+  cout << std::setprecision(20) << newton(40, 1)<<endl;
+  cout << std::setprecision(20) << newton(67, 33)<<endl;
   return RUN_ALL_TESTS();
 }
